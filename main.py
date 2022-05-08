24b4e1da186aa4b318dba3cdfb23ff45dc065b44
Tomonori HIRATA <hirata@sqlab.jp>
2022-05-08 16:56:22 +0900

    change interactive mode

--- main.py
+++ main.py
@@ -2,30 +2,37 @@ import random
 from gen import Gen
 from eval import Eval
 
-if __name__ == '__main__':
+def dpf_2party():
+    summary = """
+[2Party DPF (Distributed Point Function)]
+function f(x)
+    = b if x == a
+    = 0 else.
+λ (lmd) is input length of PRG.
+seed is PRG's seed.
+mod is modulo number.
     """
-    function f(x)
-       = b if x == a
-       = 0 else.
-    lmd is input length of PRG.
-    seed is PRG's seed.
-    """
+
+    print(summary)
+
+    print("\n====== Input Phase ======\n")
+    a = int(input("a = "))
+    b = int(input("b = "))
+    lmd = int(input("λ = "))
+    mod = int(input("mod = "))
 
     print("\n====== Gen Protocol Phase ======\n")
 
     # Gen Protocol
-    a, b = 12, 1
-    lmd = 6
-    mod = 294001
     seed = random.getrandbits(lmd)
     k0_list, k1_list, PRG = Gen.dpf_2party(a, b, lmd, seed, mod)
     print(k0_list)
     print(k1_list)
 
     print("\n====== Eval Protocol Phase ======\n")
+    x = int(input("x = "))
 
     # Eval Protocol
-    x = 14
     ans0 = Eval.dpf_2party(0, k0_list, x, lmd, mod, PRG)
     print(ans0)
     print("\n\n")
@@ -39,3 +46,6 @@ if __name__ == '__main__':
         print("f(x = {}) = 0".format(x))
     else:
         print("f(x = {}) = {}".format(x, (ans0 + ans1) % mod))
+
+if __name__ == '__main__':
+    dpf_2party()
