6426d600062dadfbf4f9d87b2bfe28b2e40034dd
Tomonori HIRATA <hirata@sqlab.jp>
2022-05-08 16:40:27 +0900

    fix: main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import random
 from gen import Gen
 from eval import Eval
 
-
 if __name__ == '__main__':
     """
     function f(x)
@@ -12,17 +11,18 @@ if __name__ == '__main__':
     seed is PRG's seed.
     """
 
+    print("\n====== Gen Protocol Phase ======\n")
+
     # Gen Protocol
     a, b = 12, 1
     lmd = 6
     mod = 294001
     seed = random.getrandbits(lmd)
-    # seed = 1
     k0_list, k1_list, PRG = Gen.dpf_2party(a, b, lmd, seed, mod)
     print(k0_list)
     print(k1_list)
 
-    print("\n===========\n")
+    print("\n====== Eval Protocol Phase ======\n")
 
     # Eval Protocol
     x = 10
@@ -32,8 +32,10 @@ if __name__ == '__main__':
     ans1 = Eval.dpf_2party(1, k1_list, x, lmd, mod, PRG)
     print(ans1)
 
+    print("\n====== Decryption Phase ======\n")
+
     # Decryption phase
     if ans0 == ans1:
-        print("\n===> f(x = {}) = 0".format(x))
+        print("f(x = {}) = 0".format(x))
     else:
-        print("\n===> f(x = {}) = {}".format(x, (ans0 + ans1) % mod))
+        print("f(x = {}) = {}".format(x, (ans0 + ans1) % mod))
